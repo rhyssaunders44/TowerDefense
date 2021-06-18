7c1555eaa9d3480b0be66770ee5f0401c2a54735
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2021-06-18 12:52:24 +1000

    put in a sphere overlap

--- TowerDefense/Assets/Towers/Wall/WallBuilder.cs
+++ TowerDefense/Assets/Towers/Wall/WallBuilder.cs
@@ -7,31 +7,23 @@ public class WallBuilder : MonoBehaviour
     [SerializeField] private GameObject ParentWall;
     [SerializeField] private GameObject ChildWall;
     [SerializeField] private float maxDistance = 7;
-    [SerializeField] private Vector3[] buildDirection;
+    [SerializeField] private Vector3 buildDirection;
     [SerializeField] private int rayCount = 8;
     [SerializeField] private LayerMask mask;
     [SerializeField] private Ray guideRay;
     void Start()
     {
-        GameObject temp = new GameObject();
-        temp.transform.parent = transform;
-
-        buildDirection = new Vector3[rayCount];
-
-        for (int i = 0; i < buildDirection.Length; i++)
+        for (int i = 0; i < 8; i++)
         {
-            temp.transform.rotation = Quaternion.AngleAxis((360 / rayCount) * i, Vector3.up);
-            buildDirection[i] = temp.transform.forward;
+            buildDirection = new Vector3(0, 0, 0);
+            guideRay = new Ray(ParentWall.transform.position, buildDirection);
 
-            guideRay = new Ray(transform.position, buildDirection[i]);
-
-            if (Physics.Raycast(guideRay, out RaycastHit hit, maxDistance))
+            if(Physics.Raycast(guideRay, out RaycastHit hit, maxDistance))
             {
-                Quaternion hitDirection = new Quaternion(360 / rayCount * i, 360 / rayCount * i, 0, 0);
+                Vector3 buildPoint = guideRay.GetPoint((ParentWall.transform.position.x + hit.point.x) / 2);
 
-                Instantiate(ChildWall, ParentWall.transform.position, hitDirection);
+                Instantiate(ChildWall, buildPoint, Quaternion.LookRotation(hit.point));
             }
         }
-        Destroy(temp);
     }
 }
